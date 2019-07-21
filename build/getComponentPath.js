8e527f59db834d9125430825a0ee0be1c94b7f52
MX J <jmx164491960@126.com>
2019-07-21 11:28:14 +0800

    getComponentPath

--- build/getComponentPath.js
+++ build/getComponentPath.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+console.log('fs:', fs);
 const getPath = function() {
     const entryDir = path.resolve(__dirname, '../packages');
-    const dirs = readdirSnyc(entryDir);
+    const dirs = fs.readdirSync(entryDir);
     console.log('dirs:', dirs, entryDir);
+    return {
+        index: entryDir
+    }
 }
 
 module.exports = getPath;
